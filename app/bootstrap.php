c0d26e3dda28c532802365d793e288ca8c8fe124
Matej Koubik <matej@koubik.name>
2013-04-26 13:16:03 +0200

    bootstrap.php load config/pagodabox.php

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -29,7 +29,7 @@ $configurator->addConfig(__DIR__ . '/config/config.neon', FALSE);
 if (file_exists(__DIR__ . '/config/config.local.neon')) {
 	$configurator->addConfig(__DIR__ . '/config/config.local.neon', FALSE);
 }
-if ($_SERVER['APP_NAME'] === 'nette-punbb-clone') {
+if (isset($_SERVER['DB!_HOST'])) {
 	$configurator->addConfig(__DIR__ . '/config/pagodabox.php', FALSE);
 }
 $container = $configurator->createContainer();
