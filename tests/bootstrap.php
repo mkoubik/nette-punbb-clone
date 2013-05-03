e092732f29d25120d66c240d98ac9a1afdbabbf5
Matej Koubik <matej@koubik.name>
2013-05-03 12:17:08 +0200

    testing environment

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -11,13 +11,18 @@ function id($val) {
 	return $val;
 }
 
+Tester\Helpers::setup();
+class_alias('Tester\Assert', 'Assert');
+define('TEMP_DIR', __DIR__ . '/tmp/' . getmygid());
+Tester\Helpers::purge(TEMP_DIR);
+
 $configurator = new Nette\Config\Configurator;
 $configurator->setDebugMode(FALSE);
-$configurator->setTempDirectory(__DIR__ . '/../temp');
+$configurator->setTempDirectory(TEMP_DIR);
 $configurator->createRobotLoader()
 	->addDirectory(__DIR__ . '/../app')
 	->register();
 
-$configurator->addConfig(__DIR__ . '/../app/config/config.neon');
-$configurator->addConfig(__DIR__ . '/../app/config/config.local.neon');
+$configurator->addConfig(__DIR__ . '/../app/config/config.neon', FALSE);
+$configurator->addConfig(__DIR__ . '/../app/config/config.local.neon', FALSE);
 return $configurator->createContainer();
